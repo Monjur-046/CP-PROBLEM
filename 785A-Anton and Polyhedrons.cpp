f1cb373de2beb0bf0df6dd54e0469f856c59f80b
Monjur E Alahi <monjuralahi283@gmail.com>
2025-12-04 13:03:19 +0600

    785A-Anton and Polyhedrons.cpp

---
+++ 785A-Anton and Polyhedrons.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    int num(0);
+    while(t--){
+        string s;
+        cin >> s;
+        if(s == "Tetrahedron") num += 4;
+        else if(s == "Cube") num += 6;
+        else if(s == "Octahedron") num += 8;
+        else if(s == "Dodecahedron") num += 12;
+        else if(s == "Icosahedron") num += 20;
+        
+        
+    }
+    cout << num << endl;
+    
+    return 0;
+} 
