2742fe2dc2bb6346aa89f731bafdee547d237d69
Monjur E Alahi <monjuralahi283@gmail.com>
2025-12-04 13:02:50 +0600

    903C - Boxes Packing.cpp

---
+++ 903C - Boxes Packing.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(0);
+    int n; cin >> n;
+    map<int, int> mp;
+    for(int i=0; i<n; i++){
+        int temp; cin >> temp;
+        mp[temp]++;
+    }
+    int mx = INT_MIN;
+    for( auto a : mp){
+        if(a.second > mx){
+            mx = a.second;
+        }
+    }
+    cout << mx << endl;
+    return 0;
+}
