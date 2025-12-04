ef382c24fcd588c25bc9b7a5b2f49cb99bdbb153
Monjur E Alahi <monjuralahi283@gmail.com>
2025-12-04 13:03:54 +0600

    766B - Mahmoud and a Triangle.cpp

---
+++ 766B - Mahmoud and a Triangle.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+	ios::sync_with_stdio(false);
+	
+	int n; cin >> n;
+	vector<int> a(n);
+	for(int i=0; i<n; i++){
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+	for(int i=1; i<n-1; i++){
+		if(a[i-1] + a[i] > a[i+1]){
+			cout<< "YES\n";
+			return 0;
+		}
+	}
+	cout << "NO\n";
+	return 0;
+}
