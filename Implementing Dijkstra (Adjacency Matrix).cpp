39a4508f081b1a9d86300de04b45b17ac54a5723
Monjur E Alahi <monjuralahi283@gmail.com>
2025-12-09 23:28:51 +0600

    Implementing Dijkstra (Adjacency Matrix).cpp

---
+++ Implementing Dijkstra (Adjacency Matrix).cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj;
+vector<int> visited;
+
+void dfs(int node) {
+    visited[node] = 1;
+    for (int nxt : adj[node]) {
+        if (!visited[nxt]) dfs(nxt);
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    adj.assign(n, {});
+    visited.assign(n, 0);
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int components = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            components++;
+            dfs(i);
+        }
+    }
+
+    cout << components << endl;
+
+    return 0;
+}
